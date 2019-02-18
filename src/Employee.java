8bb5cb496188a8ebf926253a204b6ffd761c2eb9
nwhiteman42 <nwhiteman42@gmail.com>
2019-02-18 15:21:21 -0500

    Update availability list to array since size is static

--- src/Employee.java
+++ src/Employee.java
@@ -3,5 +3,5 @@ import java.util.ArrayList;
 public class Employee {
 	String name;
 	String job;
-	ArrayList<TimeEntry> availability = new ArrayList<>();
+	TimeEntry availability[] = new TimeEntry[7];
 }
