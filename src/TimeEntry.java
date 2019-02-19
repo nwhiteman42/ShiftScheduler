04b7ce2590020f84d79fd4dbe5cb6794933a2b42
Nathan <nwhiteman42@gmail.com>
2019-02-19 17:36:16 -0500

    Create Methods for TimeEntry Class

--- src/TimeEntry.java
+++ src/TimeEntry.java
@@ -5,9 +5,45 @@ public class TimeEntry {
 	public int startTime;
 	public int endTime;
 	
+	public TimeEntry() {
+		day = "Sunday";
+		startTime = 0;
+		endTime = 2359; 
+	}
+	
 	public TimeEntry(String day, int startTime, int endTime) {
 		this.day = day;
 		this.startTime = startTime;
 		this.endTime = endTime;
+	}
+	
+	public String getDay() {
+		return day;
+	}
+	
+	private String setDay(String day) {
+		String oldDay = this.day;
+		this.day = day;
+		return oldDay;
+	}
+	
+	public int getStartTime() {
+		return startTime;
+	}
+	
+	private int setStartTime(int startTime) {
+		int oldStartTime = this.startTime;
+		this.startTime = startTime;
+		return oldStartTime;
+	}
+	
+	public int getEndTime() {
+		return endTime;
+	}
+	
+	private int setEndTime(int endTime) {
+		int oldEndTime = this.endTime;
+		this.endTime = endTime;
+		return oldEndTime;
 	}
 }
