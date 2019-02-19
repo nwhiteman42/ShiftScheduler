5175d14739251bfecb380a43accd041f5bf65b4b
Nathan <nwhiteman42@gmail.com>
2019-02-19 17:07:05 -0500

    Make constructors for Employee and TimeEntry

--- src/Employee.java
+++ src/Employee.java
@@ -1,7 +1,21 @@
 import java.util.ArrayList;
 
 public class Employee {
+	
 	String name;
 	String job;
-	TimeEntry availability[] = new TimeEntry[7];
+	TimeEntry availability[];
+	
+	public Employee() {
+		name = "Employee";
+		job = "Job";
+		availability = new TimeEntry[7];
+	}
+	
+	public Employee(String name, String job, TimeEntry availability[]) {
+		this.name = name;
+		this.job = job;
+		this.availability = availability;
+	}
+	
 }
--- src/TimeEntry.java
+++ src/TimeEntry.java
@@ -1,6 +1,13 @@
 
 public class TimeEntry {
+	
 	public String day;
 	public int startTime;
 	public int endTime;
+	
+	public TimeEntry(String day, int startTime, int endTime) {
+		this.day = day;
+		this.startTime = startTime;
+		this.endTime = endTime;
+	}
 }
