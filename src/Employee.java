38b4fa5c81dd8f61d3ce53570e23ba97a425968e
Nathan <nwhiteman42@gmail.com>
2019-02-19 17:27:47 -0500

    Create Methods for Employee Class

--- src/Employee.java
+++ src/Employee.java
@@ -1,21 +1,52 @@
-import java.util.ArrayList;
 
 public class Employee {
-	
+
 	String name;
 	String job;
 	TimeEntry availability[];
-	
+
 	public Employee() {
 		name = "Employee";
 		job = "Job";
 		availability = new TimeEntry[7];
 	}
-	
+
 	public Employee(String name, String job, TimeEntry availability[]) {
 		this.name = name;
 		this.job = job;
 		this.availability = availability;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	@SuppressWarnings("unused")
+	private String setName(String name) {
+		String oldName = this.name;
+		this.name = name;
+		return oldName;
+	}
+
+	public String getJob() {
+		return job;
+	}
+
+	@SuppressWarnings("unused")
+	private String setJob(String job) {
+		String oldJob = this.job;
+		this.job = job;
+		return oldJob;
+	}
 	
+	public TimeEntry[] getAvailability() {
+		return availability;
+	}
+	
+	@SuppressWarnings("unused")
+	private TimeEntry[] setAvailability(TimeEntry[] availability) {
+		TimeEntry[] oldAvailability = availability;
+		this.availability = availability;
+		return oldAvailability ;
+	}
 }
