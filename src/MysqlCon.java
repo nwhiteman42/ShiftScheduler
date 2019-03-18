b6f846dd7a1fafbdad980cd7c1df2c12ce1c442b
mtsayles <mtsayles@c28-0117-07.ad.mtu.edu>
2019-03-18 12:04:44 -0400

    mysqlcon: can add and delete data from database

--- src/MysqlCon.java
+++ src/MysqlCon.java
@@ -3,13 +3,33 @@
 import java.sql.*;
 
 class MysqlCon{
-	public static void main(String args[]) {
+	public void removeEmployee(int id) throws SQLException {
+	Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/DRZ3zhCKwK","DRZ3zhCKwK","JLKYtPKkBL");
+		
+		String query = "delete from Employee_Data where Employee_ID = " + id;
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.execute();
+	}
+	public void insertIntoEmployee(int id, String name, String job) throws SQLException {
+		Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/DRZ3zhCKwK","DRZ3zhCKwK","JLKYtPKkBL");
+		
+		String query = "insert into Employee_Data (Employee_ID, Employee_Name, Employee_Title)" + " values(?,?,?)";
+		PreparedStatement ps = con.prepareStatement(query);
+		ps.setInt(1,id);
+		ps.setString(2, name);
+		ps.setString(3, job);
+		
+		ps.execute();
+		con.close();
+	}
+	
+	public void retriveData() {
 		try {
 			Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/DRZ3zhCKwK","DRZ3zhCKwK","JLKYtPKkBL");
 			
 			Statement stmt = con.createStatement();
 			Statement stmt2 = con.createStatement();
-			Statement stmt3 = con.createStatement();
+			Statement stmt3 = con.createStatement(); 
 
 			ResultSet rs = stmt.executeQuery("select * from Employee_Data");
 			ResultSet rb = stmt2.executeQuery("select * from Employee_Shifts");
@@ -33,5 +53,13 @@ class MysqlCon{
 		catch(Exception e) {
 			System.out.println(e);
 		}
+	}
+	
+	
+	public static void main(String args[]) throws SQLException {
+		MysqlCon x = new MysqlCon();
+		x.insertIntoEmployee(31, "Filler", "AlbumFiller");
+		x.removeEmployee(31);
+		x.retriveData();
 	}
 }
