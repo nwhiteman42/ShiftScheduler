6d87f04b9238caa2f619d675ce970907a4f902bc
Nathan <nwhiteman42@gmail.com>
2019-03-28 17:50:40 -0400

    GUI: Have GUI make schedule

--- src/ShiftSchedulerWindow.java
+++ src/ShiftSchedulerWindow.java
@@ -3,6 +3,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JRadioButton;
 import java.awt.BorderLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ShiftSchedulerWindow {
 
@@ -38,9 +42,21 @@ public class ShiftSchedulerWindow {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new MigLayout("", "[]", "[][]"));
 		
-		JRadioButton rdbtnTest = new JRadioButton("Test");
-		frame.getContentPane().add(rdbtnTest, BorderLayout.NORTH);
+		JButton btnMakeSchedule = new JButton("Make Schedule");
+		btnMakeSchedule.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				ShiftSchedulerTests obj = new ShiftSchedulerTests();
+				try {
+					obj.addDataArray();
+				} catch (Exception e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		frame.getContentPane().add(btnMakeSchedule, "cell 0 1");
 	}
 
 }
