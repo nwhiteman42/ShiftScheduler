bfc6df179f4972eb75b7c82074b94fce30253b2f
Nathan <nwhiteman42@gmail.com>
2019-04-10 11:52:44 -0400

    EditEmployee: Complete GUI

--- src/EditEmployee.java
+++ src/EditEmployee.java
@@ -1,12 +1,9 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class EditEmployee {
 
@@ -14,6 +11,11 @@ public class EditEmployee {
 	private JTextField textField;
 	private JTextField textField_1;
 	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
 
 	/**
 	 * Launch the application.
@@ -43,7 +45,7 @@ public class EditEmployee {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 514, 394);
+		frame.setBounds(100, 100, 661, 334);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -81,5 +83,68 @@ public class EditEmployee {
 		txtrJob.setText("Job ");
 		txtrJob.setBounds(384, 46, 72, 15);
 		frame.getContentPane().add(txtrJob);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(515, 67, 124, 19);
+		frame.getContentPane().add(textField_3);
+		
+		JTextArea txtrSeniority = new JTextArea();
+		txtrSeniority.setText("Seniority");
+		txtrSeniority.setBounds(551, 46, 72, 15);
+		frame.getContentPane().add(txtrSeniority);
+		
+		JTextArea txtrNew = new JTextArea();
+		txtrNew.setText("New");
+		txtrNew.setBounds(12, 126, 31, 15);
+		frame.getContentPane().add(txtrNew);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setText("Name");
+		textArea.setBounds(57, 155, 72, 15);
+		frame.getContentPane().add(textArea);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(25, 170, 124, 19);
+		frame.getContentPane().add(textField_4);
+		
+		JTextArea textArea_1 = new JTextArea();
+		textArea_1.setText("Title");
+		textArea_1.setBounds(224, 155, 72, 15);
+		frame.getContentPane().add(textArea_1);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(189, 170, 124, 19);
+		frame.getContentPane().add(textField_5);
+		
+		JTextArea textArea_2 = new JTextArea();
+		textArea_2.setText("Job ");
+		textArea_2.setBounds(384, 155, 72, 15);
+		frame.getContentPane().add(textArea_2);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(356, 170, 124, 19);
+		frame.getContentPane().add(textField_6);
+		
+		JTextArea textArea_3 = new JTextArea();
+		textArea_3.setText("Seniority");
+		textArea_3.setBounds(551, 155, 72, 15);
+		frame.getContentPane().add(textArea_3);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(515, 170, 124, 19);
+		frame.getContentPane().add(textField_7);
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.setBounds(89, 243, 114, 25);
+		frame.getContentPane().add(btnConfirm);
+		
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.setBounds(419, 243, 114, 25);
+		frame.getContentPane().add(btnCancel);
 	}
 }
