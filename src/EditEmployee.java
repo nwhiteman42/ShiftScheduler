f7c3a4049912662ededed0779a111fd53b243f38
mtsayles <mtsayles@c28-0117-07.ad.mtu.edu>
2019-04-10 11:51:13 -0400

    Added workplace feild to create account

--- src/EditEmployee.java
+++ src/EditEmployee.java
@@ -1,10 +1,6 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
