8d1f4289705feece07c6aa06b7c6acd369192bce
Nathan <nwhiteman42@gmail.com>
2019-04-10 11:45:16 -0400

    RemoveShift: Finish Layout

---
+++ src/EditEmployee.java
@@ -0,0 +1,85 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.layout.FormSpecs;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+
+public class EditEmployee {
+
+	private JFrame frame;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					EditEmployee window = new EditEmployee();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public EditEmployee() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 514, 394);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTextArea txtrOld = new JTextArea();
+		txtrOld.setText("Old");
+		txtrOld.setBounds(12, 12, 31, 15);
+		frame.getContentPane().add(txtrOld);
+		
+		JTextArea txtrFirstName = new JTextArea();
+		txtrFirstName.setText("Name");
+		txtrFirstName.setBounds(57, 46, 72, 15);
+		frame.getContentPane().add(txtrFirstName);
+		
+		textField = new JTextField();
+		textField.setBounds(25, 67, 124, 19);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		JTextArea txtrTitle = new JTextArea();
+		txtrTitle.setText("Title");
+		txtrTitle.setBounds(224, 46, 72, 15);
+		frame.getContentPane().add(txtrTitle);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(189, 67, 124, 19);
+		frame.getContentPane().add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(356, 67, 124, 19);
+		frame.getContentPane().add(textField_2);
+		
+		JTextArea txtrJob = new JTextArea();
+		txtrJob.setText("Job ");
+		txtrJob.setBounds(384, 46, 72, 15);
+		frame.getContentPane().add(txtrJob);
+	}
+}
---
+++ src/RemoveShift.java
@@ -0,0 +1,195 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
+import javax.swing.JCheckBox;
+
+public class RemoveShift {
+
+	private JFrame frame;
+	private JTextField txtFirstName;
+	private JLabel lblFirstName;
+	private JLabel lblCreateAccount;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JTextField textField_8;
+	private JTextField textField_9;
+	private JTextField textField_10;
+	private JTextField textField_11;
+	private JTextField textField_12;
+	private JTextField textField_13;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RemoveShift window = new RemoveShift();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public RemoveShift() {
+		initialize();
+		
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 410);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		txtFirstName = new JTextField();
+		txtFirstName.setBounds(152, 72, 112, 20);
+		frame.getContentPane().add(txtFirstName);
+		txtFirstName.setColumns(10);
+		
+		lblFirstName = new JLabel("Email");
+		lblFirstName.setBounds(101, 75, 41, 14);
+		frame.getContentPane().add(lblFirstName);
+		
+		lblCreateAccount = new JLabel("Add Shift");
+		lblCreateAccount.setFont(new Font("Tahoma", Font.PLAIN, 22));
+		lblCreateAccount.setBounds(164, 25, 112, 26);
+		frame.getContentPane().add(lblCreateAccount);
+		
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.setBounds(330, 337, 89, 23);
+		frame.getContentPane().add(btnConfirm);
+		
+		JButton btnGoBack = new JButton("Go Back");
+		btnGoBack.setBounds(231, 337, 89, 23);
+		frame.getContentPane().add(btnGoBack);
+		
+		textField = new JTextField();
+		textField.setBounds(190, 121, 86, 20);
+		frame.getContentPane().add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(286, 121, 86, 20);
+		frame.getContentPane().add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblStartTime = new JLabel("Start Time");
+		lblStartTime.setBounds(200, 108, 76, 14);
+		frame.getContentPane().add(lblStartTime);
+		
+		JLabel lblEndTime = new JLabel("End Time");
+		lblEndTime.setBounds(298, 108, 74, 14);
+		frame.getContentPane().add(lblEndTime);
+		
+		JCheckBox chckbxSunday = new JCheckBox("Sunday");
+		chckbxSunday.setBounds(87, 120, 97, 23);
+		frame.getContentPane().add(chckbxSunday);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(190, 152, 86, 20);
+		frame.getContentPane().add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(190, 183, 86, 20);
+		frame.getContentPane().add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(190, 214, 86, 20);
+		frame.getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(190, 245, 86, 20);
+		frame.getContentPane().add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setColumns(10);
+		textField_6.setBounds(190, 276, 86, 20);
+		frame.getContentPane().add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setColumns(10);
+		textField_7.setBounds(190, 307, 86, 20);
+		frame.getContentPane().add(textField_7);
+		
+		textField_8 = new JTextField();
+		textField_8.setColumns(10);
+		textField_8.setBounds(286, 152, 86, 20);
+		frame.getContentPane().add(textField_8);
+		
+		textField_9 = new JTextField();
+		textField_9.setColumns(10);
+		textField_9.setBounds(286, 183, 86, 20);
+		frame.getContentPane().add(textField_9);
+		
+		textField_10 = new JTextField();
+		textField_10.setColumns(10);
+		textField_10.setBounds(286, 214, 86, 20);
+		frame.getContentPane().add(textField_10);
+		
+		textField_11 = new JTextField();
+		textField_11.setColumns(10);
+		textField_11.setBounds(286, 245, 86, 20);
+		frame.getContentPane().add(textField_11);
+		
+		textField_12 = new JTextField();
+		textField_12.setColumns(10);
+		textField_12.setBounds(286, 276, 86, 20);
+		frame.getContentPane().add(textField_12);
+		
+		textField_13 = new JTextField();
+		textField_13.setColumns(10);
+		textField_13.setBounds(286, 307, 86, 20);
+		frame.getContentPane().add(textField_13);
+		
+		JCheckBox chckbxMonday = new JCheckBox("Monday");
+		chckbxMonday.setBounds(86, 151, 97, 23);
+		frame.getContentPane().add(chckbxMonday);
+		
+		JCheckBox chckbxTuesday = new JCheckBox("Tuesday");
+		chckbxTuesday.setBounds(86, 182, 97, 23);
+		frame.getContentPane().add(chckbxTuesday);
+		
+		JCheckBox chckbxWednesday = new JCheckBox("Wednesday");
+		chckbxWednesday.setBounds(86, 213, 97, 23);
+		frame.getContentPane().add(chckbxWednesday);
+		
+		JCheckBox chckbxThursday = new JCheckBox("Thursday");
+		chckbxThursday.setBounds(87, 244, 97, 23);
+		frame.getContentPane().add(chckbxThursday);
+		
+		JCheckBox chckbxFriday = new JCheckBox("Friday");
+		chckbxFriday.setBounds(86, 275, 97, 23);
+		frame.getContentPane().add(chckbxFriday);
+		
+		JCheckBox chckbxSaturday = new JCheckBox("Saturday");
+		chckbxSaturday.setBounds(86, 306, 97, 23);
+		frame.getContentPane().add(chckbxSaturday);
+	}
+}
