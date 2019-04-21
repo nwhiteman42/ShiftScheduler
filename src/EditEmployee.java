5b1d96339986caa539ba412cea113598d6508872
mtsayles <mtsayles@c28-0112-01.ad.mtu.edu>
2019-04-21 17:08:35 -0400

    Edit Employee will now update shift accordingly

--- src/EditEmployee.java
+++ src/EditEmployee.java
@@ -12,6 +12,7 @@ import java.awt.event.ActionListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.awt.event.ActionEvent;
 
@@ -30,18 +31,16 @@ public class EditEmployee {
 		}
 	
 	private JFrame frame;
-	private JLabel txtrFirstName;
+	private JLabel txtEmail;
 	private JLabel txtrOld;
 	private JLabel txtrNew;
 	private JLabel textArea;
 	private JLabel textArea_1;
-	private JLabel textArea_3;
 	private JLabel txtrPlaceOfWork;
 	private JLabel txtrEmail;
 	private JTextField textField;
 	private JTextField textField_4;
 	private JTextField textField_5;
-	private JTextField textField_7;
 	private JTextField textField_1;
 	private JTextField textField_2;
 
@@ -90,11 +89,11 @@ public class EditEmployee {
 		txtrOld.setBounds(12, 12, 31, 15);
 		frame.getContentPane().add(txtrOld);
 		
-		txtrFirstName = new JLabel("Email");
-		txtrFirstName.setForeground(Color.WHITE);
-		txtrFirstName.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrFirstName.setBounds(45, 25, 72, 15);
-		frame.getContentPane().add(txtrFirstName);
+		txtEmail = new JLabel("Email");
+		txtEmail.setForeground(Color.WHITE);
+		txtEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
+		txtEmail.setBounds(45, 25, 72, 15);
+		frame.getContentPane().add(txtEmail);
 		
 		textField = new JTextField();
 		textField.setBounds(25, 51, 124, 19);
@@ -129,31 +128,41 @@ public class EditEmployee {
 		textField_5.setBounds(296, 91, 124, 19);
 		frame.getContentPane().add(textField_5);
 		
-		
-		textArea_3 = new JLabel("Seniority");
-		textArea_3.setForeground(Color.WHITE);
-		textArea_3.setFont(new Font("Tahoma", Font.BOLD, 13));
-		textArea_3.setBounds(310, 122, 72, 15);
-		frame.getContentPane().add(textArea_3);
-		
-		textField_7 = new JTextField();
-		textField_7.setColumns(10);
-		textField_7.setBounds(296, 138, 124, 19);
-		frame.getContentPane().add(textField_7);
-		
 		JButton btnConfirm = new JButton("Confirm");
 		btnConfirm.setBackground(Color.GRAY);
 		btnConfirm.setForeground(Color.WHITE);
 		btnConfirm.setBorder(new LineBorder(Color.WHITE));
 		btnConfirm.setFont(new Font("Tahoma", Font.BOLD, 13));
 		btnConfirm.addActionListener(new ActionListener() {
+		
 			public void actionPerformed(ActionEvent arg0){
 				MysqlCon x = new MysqlCon();
 				try {
 					//Updates the employee information
 					int id = x.getACurrentEmployeeID(textField.getText());
 					Connection con = DriverManager.getConnection("jdbc:mysql://remotemysql.com:3306/DRZ3zhCKwK","DRZ3zhCKwK","JLKYtPKkBL");
-					String query = "update Employee_Data set Employee_Name = '"+ textField_4.getText() + "', Employee_Title = '" + textField_5.getText() + "', seniority = '"+ textField_7.getText() + "', placeofwork = '" + textField_1.getText() + "', email = '" + textField_2.getText() + "' where Employee_ID = '" + id + "'";
+					//Gets previous place of work
+					String workQuery = "select placeofwork from Employee_Data where Employee_ID = "+id;
+					PreparedStatement workps = con.prepareStatement(workQuery);
+					ResultSet rs = workps.executeQuery();
+					String previousWorkplace = null;
+					while(rs.next()) {
+						previousWorkplace = rs.getString(1);
+					}
+					String newWorkplace = textField_1.getText();
+					if(previousWorkplace.equals(newWorkplace)) {
+						String query = "update Employee_Shifts set Employee_Name = ? where Employee_ID = "+ id;
+						PreparedStatement ps = con.prepareStatement(query);
+						ps.setString(1, textField_4.getText());
+						ps.executeUpdate();
+					} else {
+						String query = "delete from Employee_Shifts where Employee_ID = "+id;
+						PreparedStatement ps = con.prepareStatement(query);
+						ps.executeUpdate();
+					}
+					String query = "update Employee_Data set Employee_Name = '"+ textField_4.getText() + "', "
+							+ "Employee_Title = '" + textField_5.getText() + "', seniority = '"+ 1 + "', placeofwork = '" + 
+							textField_1.getText() + "', email = '" + textField_2.getText() + "' where Employee_ID = '" + id + "'";
 					PreparedStatement ps = con.prepareStatement(query);
 					ps.executeUpdate();
 					con.close();
@@ -185,24 +194,24 @@ public class EditEmployee {
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(296, 190, 124, 19);
+		textField_1.setBounds(296, 138, 124, 19);
 		frame.getContentPane().add(textField_1);
 		
 		txtrPlaceOfWork = new JLabel("Place of Work");
 		txtrPlaceOfWork.setForeground(Color.WHITE);
 		txtrPlaceOfWork.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrPlaceOfWork.setBounds(310, 169, 96, 15);
+		txtrPlaceOfWork.setBounds(306, 122, 96, 15);
 		frame.getContentPane().add(txtrPlaceOfWork);
 		
 		txtrEmail = new JLabel("Email");
 		txtrEmail.setForeground(Color.WHITE);
 		txtrEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrEmail.setBounds(310, 214, 96, 15);
+		txtrEmail.setBounds(310, 169, 96, 15);
 		frame.getContentPane().add(txtrEmail);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(296, 231, 124, 19);
+		textField_2.setBounds(296, 183, 124, 19);
 		frame.getContentPane().add(textField_2);
 		
 		frame.setUndecorated(true);
