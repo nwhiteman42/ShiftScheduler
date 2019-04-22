5ec8427c3311b0a223f83907d2be32a6e2b70d30
mace1099 <mtsayles@mtu.edu>
2019-04-22 01:51:25 -0400

    Edit Shift now has first name and last name field

--- src/EditEmployee.java
+++ src/EditEmployee.java
@@ -34,16 +34,17 @@ public class EditEmployee {
 	private JLabel txtEmail;
 	private JLabel txtrOld;
 	private JLabel txtrNew;
-	private JLabel textArea;
 	private JLabel textArea_1;
 	private JLabel txtrPlaceOfWork;
 	private JLabel txtrEmail;
 	private JTextField textField;
-	private JTextField textField_4;
+	private JTextField textLastName;
 	private JTextField textField_5;
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JLabel lblInvalidInput;
+	private JTextField textFirstName;
+	private JLabel lblLastName;
 
 	/**
 	 * Launch the application.
@@ -107,26 +108,20 @@ public class EditEmployee {
 		txtrNew.setBounds(227, 39, 173, 15);
 		frame.getContentPane().add(txtrNew);
 		
-		textArea = new JLabel("Name");
-		textArea.setForeground(Color.WHITE);
-		textArea.setFont(new Font("Tahoma", Font.BOLD, 13));
-		textArea.setBounds(237, 65, 72, 15);
-		frame.getContentPane().add(textArea);
-		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(237, 79, 124, 20);
-		frame.getContentPane().add(textField_4);
+		textLastName = new JTextField();
+		textLastName.setColumns(10);
+		textLastName.setBounds(237, 122, 124, 20);
+		frame.getContentPane().add(textLastName);
 		
 		textArea_1 = new JLabel("Title");
 		textArea_1.setForeground(Color.WHITE);
 		textArea_1.setFont(new Font("Tahoma", Font.BOLD, 13));
-		textArea_1.setBounds(237, 121, 72, 15);
+		textArea_1.setBounds(237, 153, 72, 15);
 		frame.getContentPane().add(textArea_1);
 		
 		textField_5 = new JTextField();
 		textField_5.setColumns(10);
-		textField_5.setBounds(237, 136, 124, 20);
+		textField_5.setBounds(237, 167, 124, 20);
 		frame.getContentPane().add(textField_5);
 		
 		lblInvalidInput = new JLabel("Invalid Input");
@@ -145,7 +140,8 @@ public class EditEmployee {
 		public void actionPerformed(ActionEvent arg0){
 				MysqlCon x = new MysqlCon();
 				if(textField_1.getText().isEmpty() || textField_5.getText().isEmpty() || textField.getText().isEmpty()||
-					textField_4.getText().isEmpty() || textField_2.getText().isEmpty()) {
+					textLastName.getText().isEmpty() || textField_2.getText().isEmpty() || textFirstName.getText().isEmpty() ||
+					textLastName.getText().isEmpty()) {
 					lblInvalidInput.setForeground(Color.RED);
 					return;
 				}
@@ -165,14 +161,14 @@ public class EditEmployee {
 					if(previousWorkplace.equals(newWorkplace)) {
 						String query = "update Employee_Shifts set Employee_Name = ? where Employee_ID = "+ id;
 						PreparedStatement ps = con.prepareStatement(query);
-						ps.setString(1, textField_4.getText());
+						ps.setString(1, textLastName.getText());
 						ps.executeUpdate();
 					} else {
 						String query = "delete from Employee_Shifts where Employee_ID = "+id;
 						PreparedStatement ps = con.prepareStatement(query);
 						ps.executeUpdate();
 					}
-					String query = "update Employee_Data set Employee_Name = '"+ textField_4.getText() + "', "
+					String query = "update Employee_Data set Employee_Name = '"+ textFirstName.getText() +" "+textLastName.getText() + "', "
 							+ "Employee_Title = '" + textField_5.getText() + "', seniority = '"+ 1 + "', placeofwork = '" + 
 							textField_1.getText() + "', email = '" + textField_2.getText() + "' where Employee_ID = '" + id + "'";
 					PreparedStatement ps = con.prepareStatement(query);
@@ -206,25 +202,42 @@ public class EditEmployee {
 		
 		textField_1 = new JTextField();
 		textField_1.setColumns(10);
-		textField_1.setBounds(237, 191, 124, 20);
+		textField_1.setBounds(237, 212, 124, 20);
 		frame.getContentPane().add(textField_1);
 		
 		txtrPlaceOfWork = new JLabel("Place of Work");
 		txtrPlaceOfWork.setForeground(Color.WHITE);
 		txtrPlaceOfWork.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrPlaceOfWork.setBounds(237, 176, 96, 15);
+		txtrPlaceOfWork.setBounds(237, 198, 96, 15);
 		frame.getContentPane().add(txtrPlaceOfWork);
 		
 		txtrEmail = new JLabel("Email");
 		txtrEmail.setForeground(Color.WHITE);
 		txtrEmail.setFont(new Font("Tahoma", Font.BOLD, 13));
-		txtrEmail.setBounds(237, 235, 96, 15);
+		txtrEmail.setBounds(237, 243, 96, 15);
 		frame.getContentPane().add(txtrEmail);
 		
 		textField_2 = new JTextField();
 		textField_2.setColumns(10);
-		textField_2.setBounds(237, 249, 124, 20);
+		textField_2.setBounds(237, 260, 124, 20);
 		frame.getContentPane().add(textField_2);
+		
+		textFirstName = new JTextField();
+		textFirstName.setColumns(10);
+		textFirstName.setBounds(237, 79, 124, 20);
+		frame.getContentPane().add(textFirstName);
+		
+		JLabel lblFirstName = new JLabel("First Name");
+		lblFirstName.setForeground(Color.WHITE);
+		lblFirstName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblFirstName.setBounds(237, 66, 72, 15);
+		frame.getContentPane().add(lblFirstName);
+		
+		lblLastName = new JLabel("Last Name");
+		lblLastName.setForeground(Color.WHITE);
+		lblLastName.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblLastName.setBounds(237, 110, 72, 15);
+		frame.getContentPane().add(lblLastName);
 		
 		
 		
